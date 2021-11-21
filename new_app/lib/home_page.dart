ea779f0c744b010c11a8c765409af401f24753cf
Mou Biswas <qtmeaowmou16@gmail.com>
2021-11-22 00:07:53 +0600

    init

---
+++ new_app/lib/home_page.dart
@@ -0,0 +1,74 @@
+// ignore_for_file: prefer_const_constructors, duplicate_ignore
+
+import 'package:flutter/material.dart';
+import 'package:new_app/screens/all.dart';
+import 'package:new_app/screens/business.dart';
+import 'package:new_app/screens/gadgets.dart';
+import 'package:new_app/screens/sports.dart';
+
+class HomeScreen extends StatelessWidget {
+  static const String path = "HomeScreen";
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+      length: 4,
+      child: Scaffold(
+        // ignore: duplicate_ignore, duplicate_ignore
+        appBar: AppBar(
+          centerTitle: true,
+          elevation: 10,
+          backgroundColor: Colors.white,
+          // ignore: prefer_const_constructors
+          iconTheme: IconThemeData(
+            color: Colors.black,
+          ),
+          title: Text(
+            "InterNational News",
+            style: TextStyle(
+              color: Colors.black,
+              fontWeight: FontWeight.w700,
+            ),
+          ),
+          actions: [
+            IconButton(
+              onPressed: () {},
+              icon: Icon(
+                Icons.notification_important,
+              ),
+            )
+          ],
+        ),
+        drawer: Drawer(),
+        body: Column(
+          children: const [
+            TabBar(
+              labelPadding: EdgeInsets.all(10),
+              labelColor: Colors.blue,
+              indicatorColor: Colors.black,
+              indicatorSize: TabBarIndicatorSize.tab,
+              unselectedLabelColor: Colors.grey,
+              tabs: [
+                Text("All"),
+                Text("Business"),
+                Text("Gadgets"),
+                Text("Sports"),
+              ],
+            ),
+            Expanded(
+              child: TabBarView(
+                children: [
+                  AllScreen(),
+                  BusinessScreen(),
+                  GadgetsScreen(),
+                  SportsScreen(),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
